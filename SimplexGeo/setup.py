fb67e3bbed272bdfbcbd509ebc9a1077e116e39b
Henry Robbins <hwr26@cornell.edu>
2020-06-29 16:47:42 -0400

    changed URL

--- SimplexGeo/setup.py
+++ SimplexGeo/setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="A package for visualizing the simplex algorithm.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/henryrobbins/simplex-geo.git/",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
