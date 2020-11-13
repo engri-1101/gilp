Bug fixes: numerical issue in polytope_facets & params not passed to bfs_plot.

gilp/geometry.py
@@ -1,12 +1,12 @@
 """Computational geometry functions.
 
 This module contains various computational geometry functions related to linear
-programming. The halfspace intersection functions is a high-level interface
-with the pyhull package which is a Python wrapper for Qhull.
+programming.
 """
 
 __author__ = 'Henry Robbins'
-__all__ = ['intersection', 'halfspace_intersection', 'interior_point', 'order']
+__all__ = ['intersection', 'polytope_vertices', 'polytope_facets',
+           'halfspace_intersection', 'interior_point', 'order']
 
 from collections import namedtuple
 import itertools
@@ -127,7 +127,7 @@ def polytope_facets(A: np.ndarray,
     for v in vertices:
         on_facet = np.isclose(a=np.matmul(A, v) - b,
                               b=np.zeros((len(A),1)),
-                              atol=1e-12)
+                              atol=1e-10)
         defining_facets.append(np.where(on_facet)[0])
     facets = []
     for i in range(len(A)):

gilp/visualize.py
@@ -17,7 +17,7 @@ import numpy as np
 import plotly.graph_objects as plt
 from typing import Union, List, Tuple
 from .geometry import (intersection, halfspace_intersection, interior_point,
-                    polytope_vertices, polytope_facets, NoInteriorPoint,)
+                       polytope_vertices, NoInteriorPoint,)
 from .graphic import (num_format, equation_string, linear_string, plot_tree,
                       Figure, label, table, vector, scatter, equation, polygon,
                       polytope)
@@ -386,7 +386,10 @@ def add_feasible_region(fig: Figure,
     A_tmp = np.vstack((A,-np.identity(n)))
     b_tmp = np.vstack((b,np.zeros((n,1))))
     vertices = polytope_vertices(A_tmp, b_tmp)
-    fig.add_trace(bfs_plot(lp, vertices))
+    fig.add_trace(bfs_plot(lp=lp,
+                           basic_sol=basic_sol,
+                           show_basis=show_basis,
+                           vertices=vertices))
 
     if set_axes:
         x_list = [list(x[:,0]) for x in vertices]